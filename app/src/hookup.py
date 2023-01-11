0f0721097111883fd0a0a76496b11533dd73e9a4
Stallone Davide (stlo) <stlo@zhaw.ch>
2023-01-11 16:34:08 +0100

    Add hookup.py and read AD credentials from secrets.json

---
+++ app/src/hookup.py
@@ -0,0 +1,54 @@
+import datetime
+import logging
+import db
+import ldap
+import settings
+import utils
+
+
+logger = logging.getLogger("ad_resolver")
+
+
+def run():
+    if not ldap.connected():
+        logger.debug("Connecting to active directory")
+        ldap.connect()
+
+    logger.debug(f"Querying next unchecked author batch of size {settings.BATCH_SIZE}")
+    for author in db.queryUncheckedAuthorBatch():
+        logger.debug(f"Processing author: {author}")
+        person_update = None
+
+        if author["person"]:
+            if not author["person"]["ad_check"]:
+                logger.debug(f"Searching active directory for person with dn {author['person']['LDAPDN']}")
+                status, result, response = ldap.find_person_by_dn(author["person"]["LDAPDN"])
+                if len(response):
+                    person_update = utils.read_person_values_from_ad_entry(response[0])
+                else:
+                    person_update = { "LDAPDN": author["person"]["LDAPDN"], "retired": 1 }
+        else:
+            for surname, given_name in utils.build_full_name_variations(author["fullname"]):
+                logger.debug("Searching active directory for person with sn {surname} and givenName {given_name}")
+                status, result, response = ldap.find_person_by_surename_and_given_name(surname, given_name)
+                if len(response):
+                    # TODO: Decide what to do if more than 1 match is returned.
+                    person_update = utils.read_person_values_from_ad_entry(response[0])
+                    break
+
+        update_time = int(datetime.datetime.now(datetime.timezone.utc).timestamp())
+        author_update = { "ad_check": update_time }
+
+        if person_update:
+            author_update["person"] = { "LDAPDN": person_update["LDAPDN"] }
+        
+        logger.debug(f"Updating author: {author_update}")
+        result = db.updateAuthor(author["fullname"], author_update)
+
+        if person_update:
+            person_update["ad_check"] = update_time
+            person_ldapdn = person_update["LDAPDN"]
+            logger.debug(f"Updating person: {person_update}")
+            del person_update["LDAPDN"]
+            # This relys on updateAuthor() creating non existing person objects.
+            db.updatePerson(person_ldapdn, person_update)
--- app/src/settings.py
+++ app/src/settings.py
@@ -1,4 +1,13 @@
+import json
 import os
+
+
+if os.path.exists('/etc/app/secrets.json'):
+    with open('/etc/app/secrets.json') as secrets_file:
+        secrets = json.load(secrets_file)
+        for key in ['ad_user', 'ad_pass']:
+            if key in secrets and secrets[key] is not None:
+                os.environ[str.upper(key)] = secrets[key]
 
 AD_HOST = os.getenv('AD_HOST', 'zhaw.ch')
 AD_USER = os.getenv('AD_USER')
