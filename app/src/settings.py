4481b4a69047366d360442292deadc7d3b975171
Christian Glahn <cpglahn@gmail.com>
2023-01-30 11:30:01 +0100

    load ad_host from config file

--- app/src/settings.py
+++ app/src/settings.py
@@ -4,7 +4,7 @@ import os
 if os.path.exists('/etc/app/secrets.json'):
     with open('/etc/app/secrets.json') as secrets_file:
         config = json.load(secrets_file)
-        params = ['ad_user', 'ad_pass', 'db_host', 'log_level', 'batch_size', 'batch_interval']
+        params = ['ad_user', 'ad_pass', 'ad_host', 'db_host', 'log_level', 'batch_size', 'batch_interval']
         for key in params:
             if key in config and config[key] is not None:
                 os.environ[str.upper(key)] = config[key]
