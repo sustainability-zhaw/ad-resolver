780bb179a65a8e288772f2029148f73585efdd66
Davide Stallone <stlo@zhaw.ch>
2023-04-28 11:43:08 +0200

    Refactor building full name variations

--- app/src/utils.py
+++ app/src/utils.py
@@ -1,54 +1,36 @@
 import itertools
+import re
 
 
-def clean_words(words):
-    cleaned_words = []
-
-    for word in words:
-        if word.endswith('.'):
-            continue    
-        for char in ['(', ')']:
-            word = word.replace(char, '')
-        cleaned_words.append(word)
-
-    return cleaned_words
-
-
-def build_full_name_variations(full_name):
-    def build_variations(value):
-        variations = []
-        initial_words = [word for word in value.split(' ')]
-        cleaned_words = clean_words(initial_words)
-
-        for word in cleaned_words:
-            if '-' in word:
-                variations.extend(word.split('-'))
-
-        if len(initial_words) > 1:
-            variations.extend(cleaned_words)
-
-        if len(cleaned_words) > 1:
-            variations.append('-'.join(cleaned_words))
-
-        return variations
-
-    nameparts = [value.strip() for value in full_name.split(',')]
-
-    if len(nameparts) != 2: 
+def build_full_name_variations(full_name: str):
+    surname_and_given_name = [value.strip() for value in full_name.split(',')]
+    
+    if len(surname_and_given_name) != 2:
         return []
 
-    surname, given_name = nameparts
-    
+    surname_and_given_name_words = [None, None]
+
+    for index, value in enumerate(surname_and_given_name):
+        # Matches unicode characters
+        words = re.findall(r'[\w+-]+\b(?!\.)', value) 
+        
+        for word in words:
+            if '-' in word: 
+                words.extend(word.split('-'))
+
+        # Adding original name part back
+        if value not in words:
+            words = [value] + words 
+        
+        surname_and_given_name_words[index] = words
+
     full_name_variations = list(itertools.product(
-        [surname] + build_variations(surname),
-        [given_name] + build_variations(given_name)
+        surname_and_given_name_words[0],
+        surname_and_given_name_words[1]
     ))
 
-    if len(full_name_variations) > 2:
-        full_name_tuple = full_name_variations[0]
-        full_name_variations = full_name_variations[1:]
-        full_name_variations.sort(key=lambda values: len(values[0] + values[1]), reverse=True)
-        full_name_variations.insert(0, full_name_tuple)
+    # TODO: Put input value at the top and sort the rest
+    full_name_variations.sort(key=lambda values: len(values[0] + values[1]), reverse=True)
 
     return full_name_variations
 
